dcb768001dbb8cdf6dd18aab92c00a4fc862adee
stefaneacsu147 <stefaneacsu@gmail.com>
2018-05-17 21:04:38 +0300

    Added input for mobile #2

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var mainState = {
 
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
-        spaceKey.events.onInputDown.add(this.jump, this);
+        spaceKey.onInputDown.add(this.jump, this);
 
         this.pipes = game.add.group();
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
